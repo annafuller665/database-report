43a18caab5479660478afcb764ba12b5c95dcb81
Anna Fuller <annafuller665@gmail.com>
2023-02-15 14:21:33 -0800

    updated database scripts

--- database_report.py
+++ database_report.py
@@ -7,8 +7,14 @@ df_strains = pd.read_csv("/Users/phillipscitp/Desktop/database-report/all_strain
 df_strains_prev = pd.read_csv("/Users/phillipscitp/Desktop/database-report/all_strains_previous.csv")
 df_comps = pd.read_csv("/Users/phillipscitp/Desktop/database-report/all_compounds.csv")
 df_comps_prev = pd.read_csv("/Users/phillipscitp/Desktop/database-report/all_compounds_previous.csv")
+celest = pd.read_csv("/Users/phillipscitp/Desktop/database-report/tables/celest_experiment_table.csv")
+celest_prev = pd.read_csv("/Users/phillipscitp/Desktop/database-report/tables/celest_experiment_table_previous.csv")
+xcomps = pd.read_csv("/Users/phillipscitp/Desktop/database-report/tables/xcomps.csv")
+xcomps_prev = pd.read_csv("/Users/phillipscitp/Desktop/database-report/tables/xcomps_previous.csv")
+xstrains = pd.read_csv("/Users/phillipscitp/Desktop/database-report/tables/xstrainthaws.csv")
+xstrains_prev = pd.read_csv("/Users/phillipscitp/Desktop/database-report/tables/xstrainthaws_previous.csv")
 
-# Experiment Data report
+# Experiment Data report single file
 
 def experiment_data(df):
     total_exp = len(df)
@@ -63,8 +69,8 @@ def create_txt(desired_filename: str, func, df):
     the_file = open(desired_filename, "w")
     write_file = the_file.write(f"{func(df)}")
     the_file.close()
-
-def experiment_data_comparison(df1, df2, df3, df4, df5, df6, df7, df8):
+# Comparison data compiler
+def experiment_data_comparison(df1, df2, df3, df4, df5, df6, df7, df8, df9, df10, df11, df12, df13, df14):
     """Comparing previous and current experiment summaries"""
     total_expa = len(df1)
     total_expb = len(df2)
@@ -137,42 +143,62 @@ def experiment_data_comparison(df1, df2, df3, df4, df5, df6, df7, df8):
             new_plate_ids.append(id)
     total_strains = len(df5)
     new_strains = total_strains - len(df6)
-    new_strains_name = []
-    new_strains_id = []
-    for name in df5["Strain"]:
-        if name not in df6["Strain"]:
-            new_strains_name.append(name)
-    for name in df5["Strain ID"]:
-        if name not in df6["Strain ID"]:
-            new_strains_id.append(name)
+    
     total_comp = len(df7)
     new_comp = total_comp - len(df8)
-    new_comp_names = []
-    for name in df7["Compound"]:
-        if name not in df8["Compound"]:
-            new_comp_names.append(name)
-    new_comp_ids = []
-    for id in df7["Compound ID"]:
-        if id not in df8["Compound ID"]:
-            new_comp_ids.append(id)
+    total_celest = len(df9)
+    new_celest = total_celest - len(df10)
+    total_xcomps = len(df11)
+    new_xcomps = total_xcomps - len(df12)
+    total_xstrains = len(df13)
+    new_xstrains = total_xstrains - len(df14)
+
+    
     
     
 
 
-    data = {"Total Experiments": total_expa, "New Experiments": total_expa-total_expb, "Total Experiments Completed": total1a, "New Experiments Completed": total1a -total1b, "Total Metadata Completed": total2a, "New Metadata Completed": total2a-total2b, "Total Experiments Validated": total3a, "New Experiments Validated": total3a-total3b, "Total Phillips Lab": phillips_a, "New Phillips Lab": phillips_a-phillips_b, "Total Lithgow Lab": lithgow_a, "New Lithgow Lab": lithgow_a-lithgow_b, "Total Driscoll Lab": driscoll_a, "New Driscoll Lab": driscoll_a-driscoll_b, "Most Recent Start Date": recent_datea, "Total Manual Lifespan Types": manual_a, "New Manual Lifespan Types": manual_a-manual_b, "Total ALM Lifepsan Types": alm_a, "New ALM Lifespan Types": alm_a-alm_b, "Total Plates": total_plates, "New Plates": new_plates, "New Plate IDs": new_plate_ids, "Total Strains": total_strains, "New Strains": new_strains, "New Strain Names": new_strains_name, "New Strain IDs": new_strains_id, "Total Compounds": total_comp, "New Compounds": new_comp, "New Compound Names": new_comp_names, "New Compound IDs": new_comp_ids}                      
+    data = {"Total Experiments": total_expa, "New Experiments": total_expa-total_expb, "Total Experiments Completed": total1a, "New Experiments Completed": total1a -total1b, "Total Metadata Completed": total2a, "New Metadata Completed": total2a-total2b, "Total Experiments Validated": total3a, "New Experiments Validated": total3a-total3b, "Total Phillips Lab": phillips_a, "New Phillips Lab": phillips_a-phillips_b, "Total Lithgow Lab": lithgow_a, "New Lithgow Lab": lithgow_a-lithgow_b, "Total Driscoll Lab": driscoll_a, "New Driscoll Lab": driscoll_a-driscoll_b, "Most Recent Start Date": recent_datea, "Total Manual Lifespan Types": manual_a, "New Manual Lifespan Types": manual_a-manual_b, "Total ALM Lifepsan Types": alm_a, "New ALM Lifespan Types": alm_a-alm_b, "Total Plates": total_plates, "New Plates": new_plates, "Total Strains": total_strains, "New Strains": new_strains, "Total Compounds": total_comp, "New Compounds": new_comp, "Total Celest Experiments": total_celest, "New Celest Experiments": new_celest, "Total Xcomp Experiments": total_xcomps, "New Xcomp Experiments": new_xcomps, "Total Xstrain Experiments": total_xstrains, "New Xstrain Experiments": new_xstrains}                      
     compiler_df = pd.DataFrame.from_dict(data, orient="index")
     
     
+    
     return compiler_df
+ # New Strain Compiler   
+def new_strains(df1, df2):
+    new_strains_name = []
+    new_strains_id = []
+    for name in df1["Strain"]:
+        if name not in df2["Strain"]:
+            new_strains_name.append(name)
+    for name in df1["Strain ID"]:
+        if name not in df2["Strain ID"]:
+            new_strains_id.append(name)
+    data = {"New Strain Names": new_strains_name, "New Strain IDs": new_strains_id}
+    strain_names_df = pd.DataFrame.from_dict(data, orient = "columns")
+    return strain_names_df
+new_strains(df_strains, df_strains_prev).to_csv("new_strains_ids.csv")
 
-    # return f"In the recent experiment data set there are {total_expa - total_expb} more total experiments, {total1a -total1b} more completed experiments, {total2a - total2b} more metadata complete, and {total3a - total3b} more experiments validated. In the Phillips lab, {phillips_a - phillips_b} more experiments were conducted. In the Lithgow lab, {lithgow_a - lithgow_b} more experiments were conducted. In the Driscoll lab, {driscoll_a - driscoll_b} more experiment was conducted. The most recent start date in the previous dataset is {recent_datea} and the most recent start date in the current dataset is {recent_dateb}. In the recent dataset, there are {manual_a - manual_b} more manual lifespan experiment types and {alm_a - alm_b} more alm lifespan experiment types."
+# New Compound Compiler
+def new_compounds(df1, df2):
+    new_comp_names = []
+    for name in df1["Compound"]:
+        if name not in df2["Compound"]:
+            new_comp_names.append(name)
+    new_comp_ids = []
+    for id in df1["Compound ID"]:
+        if id not in df2["Compound ID"]:
+            new_comp_ids.append(id)
+    data = {"New Compound Names": new_comp_names, "New Compound IDs": new_comp_ids}
+    compound_names_df = pd.DataFrame.from_dict(data, orient="columns")
+    return compound_names_df
+new_compounds(df_comps, df_comps_prev).to_csv("new_compounds_names_ids.csv")
 
-experiment_data_comparison(df_experiments, df_experiments_prev, df_plates1, df_plates2, df_strains, df_strains_prev, df_comps, df_comps_prev).to_csv("experiment_data_summary.csv")
+experiment_data_comparison(df_experiments, df_experiments_prev, df_plates1, df_plates2, df_strains, df_strains_prev, df_comps, df_comps_prev, celest, celest_prev, xcomps, xcomps_prev, xstrains, xstrains_prev).to_csv("experiment_data_summary.csv")
 
+# Plate Data Comparison Compiler
 def plate_data_comparison(df1, df2):
     total_plates1 = len(df1)
     total_plates2 = len(df2)
     return f"There are {total_plates1 - total_plates2} more plates included in the recent plate dataset."
 
-# print(plate_data_comparison(df_plates1, df_plates2))
-# "New Experiments added", "Total Experiments Completed", "New Completed Experiments", "Total Metadata Completd", "New Metadata Completed", "Total Experiments Validated", "New Experiments Validated", "Total Phillips Lab", "New Phillips Lab", "Total Lithgow Lab", "New Lithgow Lab", "Total Driscoll Lab", "New Driscoll Lab", "Most Recent Start Date", "Total Manual Lifespan Types", "New Manual Lifespan Types", "Total ALM Lifespan Types", "New ALM Lifespan Types"
